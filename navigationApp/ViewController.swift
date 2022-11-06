92ecc149a328b5c2df8ea6796612adc6001a993c
krisuv16 <krisuv16@gmail.com>
2022-11-06 18:27:41 -0500

    Initial Commit

---
+++ navigationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigationApp
+//
+//  Created by Krisuv Bohara on 2022-11-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
